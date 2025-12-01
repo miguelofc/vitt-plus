14de4cc775b9664452f158994bbe3497e9caeb09
Miguel Ângelo <miguel@MacBook-Miguel.local>
2025-12-01 14:56:32 -0300

    Corrige environment de produção

--- frontend/src/app/environments/environment.prod.ts
+++ frontend/src/app/environments/environment.prod.ts
@@ -1,4 +1,14 @@
 export const environment = {
-  production: false,
+  production: true,
+
   backendUrl: "https://vitt-plus-backend.onrender.com",
+
+  firebaseConfig: {
+    apiKey: "AIzaSyDHqxBF-ZLypbyJ-LIC4IW44SungfYQRdo",
+    authDomain: "vittaplus-app.firebaseapp.com",
+    projectId: "vittaplus-app",
+    storageBucket: "vittaplus-app.firebasestorage.app",
+    messagingSenderId: "90206602420",
+    appId: "1:90206602420:web:505575214dd932932bc3e1"
+  }
 };
