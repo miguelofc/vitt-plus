1615d7446b02677fce61cd511990e34efe3283ce
Miguel Ângelo <miguel@MacBook-Miguel.local>
2025-12-01 18:38:38 -0300

    Corrige WebRTC e Firestore no frontend

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -7,9 +7,19 @@ import { appRoutes } from './app/app.routes';
 
 import { authInterceptor } from './app/core/interceptors/auth.interceptor';
 
+// Firebase
+import { provideFirebaseApp } from '@angular/fire/app';
+import { initializeApp } from 'firebase/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { environment } from './app/environments/environment';
+
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(appRoutes),
     provideHttpClient(withInterceptors([authInterceptor])),
+
+    // Firebase compatível com Angular 17
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideFirestore(() => getFirestore()),
   ],
 });
